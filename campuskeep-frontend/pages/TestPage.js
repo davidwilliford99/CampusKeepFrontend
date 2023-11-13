230c25f8c37d370fb3ca5bb47ecd6ef8274a3c53
davidwilliford99 <d9899w@gmail.com>
2023-11-13 18:43:01 -0500

    Still struggling with NavBar

--- campuskeep-frontend/pages/TestPage.js
+++ campuskeep-frontend/pages/TestPage.js
@@ -1,25 +1,12 @@
-import { Box, Button, Text } from '@chakra-ui/react';
-
-
+import react from 'react';
 
 export default function TestPage() {
 
   return (
-
-    <Box className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-
-      <Text fontSize="4xl" className="mb-4">
-        Welcome to the Homepage
-      </Text>
-
-      <Text fontSize="md" className="mb-8">
-        This is a Next.js homepage using Chakra UI and Tailwind CSS.
-      </Text>
-
-      <Button colorScheme="blue" className="mt-4">
-        Get Started
-      </Button>
-      
-    </Box>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   );
 }
--- campuskeep-frontend/src/app/page.js
+++ campuskeep-frontend/src/app/page.js
@@ -1,6 +1,6 @@
 import react from "react"
 import HeroSection from '../../components/HeroSection.js'
-import Navbar from '../../components/Navbar.client.js'
+import Navbar from '../../components/Navbar.js'
 
 export default function Home() {
   return (
