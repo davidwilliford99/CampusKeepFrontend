49e6a5922526b5a0a33a98ab742177d43150c23b
davidwilliford99 <d9899w@gmail.com>
2023-11-14 11:15:47 -0500

    Changed Tailwind Config to work correctly

--- campuskeep-frontend/components/Navbar.js
+++ campuskeep-frontend/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
           <div className="flex space-x-7">
             <div>
               {/* Website Logo */}
-              <Link href="/">
+              <Link legacyBehavior href="/">
                 <a className="flex items-center py-4 px-2">
                   <span className="font-semibold text-gray-500 text-lg">Campus Keep</span>
                 </a>
@@ -17,14 +17,14 @@ const Navbar = () => {
             </div>
             {/* Primary Navbar items */}
             <div className="hidden md:flex items-center space-x-1">
-              <Link href="/"><a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Home</a></Link>
-              <Link href="/lost-items"><a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Lost Items</a></Link>
-              <Link href="/categories"><a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Categories</a></Link>
+              <Link legacyBehavior href="/"><a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Home</a></Link>
+              <Link legacyBehavior href="/lost-items"><a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Lost Items</a></Link>
+              <Link legacyBehavior href="/categories"><a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Categories</a></Link>
             </div>
           </div>
           {/* Secondary Navbar items */}
           <div className="hidden md:flex items-center space-x-3 ">
-            <Link href="/sign-up"><a className="py-2 px-2 font-medium text-gray-500 hover:text-gray-700 transition duration-300">Sign Up / Sign In</a></Link>
+            <Link legacyBehavior href="/sign-up"><a className="py-2 px-2 font-medium text-gray-500 hover:text-gray-700 transition duration-300">Sign Up / Sign In</a></Link>
           </div>
         </div>
       </div>
