a8d9e83fa8add9e0abaaa959fb4eb91621b84a24
mjseetoo <micah.seetoo@gmail.com>
2023-11-16 10:35:43 -0500

    styling changes

--- campuskeep-frontend/components/HeroSection.js
+++ campuskeep-frontend/components/HeroSection.js
@@ -19,7 +19,7 @@ const HeroSection = () => {
       <VStack textAlign="center" color="white" p={4}>
         <Text fontSize='5xl' fontWeight="bold">Campus Keep</Text>
         <Text fontSize='lg' fontWeight="light">Keeping your lost items safe</Text>
-        <Link legacyBehavior href="/UploadItem"><a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Click here to upload an Item</a></Link>
+        <Link legacyBehavior href="/UploadItem"><a className="py-4 px-2 text-purple-500 hover:text-gray-700 transition duration-300 font-bold">Click here to upload an Item</a></Link>
       </VStack>
     </Box>
   );
