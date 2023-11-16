a83069234ef1e9d7a0d73f4895027a4409f1924b
mjseetoo <micah.seetoo@gmail.com>
2023-11-16 10:33:11 -0500

    styling changes

--- campuskeep-frontend/components/Navbar.js
+++ campuskeep-frontend/components/Navbar.js
@@ -1,23 +1,37 @@
-// use client
+// use client 
+//here is davids campus keep link if you want it.
+/*<Link legacyBehavior href="/">
+                <a className="flex items-center py-4 px-2">
+                  <span className="font-semibold text-gray-500 text-lg">Campus Keep</span>
+                </a>
+              </Link>*/
 
 import React from 'react';
 import Link from 'next/link';
 import SignUp from '../pages/SignUp';
+import { Box, Text, VStack, ChakraProvider } from '@chakra-ui/react';
 
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className="bg-purple-300 shadow-lg">
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-between">
           <div className="flex space-x-7">
             <div>
-              {/* Website Logo */}
-              <Link legacyBehavior href="/">
-                <a className="flex items-center py-4 px-2">
-                  <span className="font-semibold text-gray-500 text-lg">Campus Keep</span>
-                </a>
-              </Link>
+            <Box
+        w="200px"
+        h="80px"
+        bgImage="url('/images/campuskeep-removebg.png')"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+      </Box>
+              
             </div>
             {/* Primary Navbar items */}
             <div className="hidden md:flex items-center space-x-1">
