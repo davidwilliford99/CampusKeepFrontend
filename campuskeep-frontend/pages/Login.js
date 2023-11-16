e9b8c05a48b8a530c621a8060a8e9db1737bce99
mjseetoo <micah.seetoo@gmail.com>
2023-11-16 09:17:02 -0500

    styling added to login and SignUp

--- campuskeep-frontend/pages/Login.js
+++ campuskeep-frontend/pages/Login.js
@@ -18,36 +18,47 @@ function Login() {
   };
 
   return (
-    <div className="Login">
+    <div className="flex items-center justify-center h-screen bg-purple-900 text-yellow-200">
       {loggedIn ? (
         <div>
-          <h1>Welcome, {username}!</h1>
+          <h1 className="text-4xl font-bold">Welcome, {username}!</h1>
           <p>You are now logged in.</p>
         </div>
       ) : (
-        <div>
-          <h1>Login Page</h1>
-          <label>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">Login Page</h1>
+          <label className="block mb-2">
             Username:
             <input
+              className="border p-2 rounded"
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
           </label>
           <br />
-          <label>
+          <label className="block mb-2">
             Password:
             <input
+              className="border p-2 rounded"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </label>
           <br />
-          <button onClick={handleLogin}>Login</button>
+          <button
+            className="bg-yellow-400 text-purple-900 py-2 px-4 rounded"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
           <br />
-          <Link legacyBehavior href="SignUp"><a className="py-2 px-2 font-medium text-gray-500 hover:text-gray-700 transition duration-300">Don't have an account? Click here</a></Link>
+          <Link href="/SignUp" legacyBehavior>
+            <a className="py-2 px-2 font-medium text-gray-500 hover:text-gray-700 transition duration-300">
+              Don't have an account? Click here
+            </a>
+          </Link>
         </div>
       )}
     </div>
--- campuskeep-frontend/pages/SignUp.js
+++ campuskeep-frontend/pages/SignUp.js
@@ -13,22 +13,26 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSignUp}>
-        <label>
+    <div className='fixed top-0 left-0 h-screen w-1/2 m-2
+                    flex flex-col items-center justify-center
+                    bg-purple-800 text-yellow-200'>
+      <h2 className='text-4xl text-purple-500 font-bold mb-6'>Sign Up</h2>
+      <form className='text-yellow-400' onSubmit={handleSignUp}>
+        <label className='mb-4'>
           Email:
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input className='border p-2 rounded' type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
         <br />
-        <label>
+        <label className='mb-4'>
           Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input className='border p-2 rounded' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </label>
         <br />
-        <button type="submit">Sign Up</button>
+        <button className='bg-yellow-400 text-purple-900 py-2 px-4 rounded' type="submit">Sign Up</button>
         <br />
-        <Link legacyBehavior href="/Login"><a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Already have an account.</a></Link>
+        <Link href="/Login" legacyBehavior>
+      <a className="py-4 px-2 text-gray-500 hover:text-gray-700 transition duration-300">Already have an account.</a>
+    </Link>
       </form>
     </div>
   );
