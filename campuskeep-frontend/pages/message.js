6894b88d84820176e31101a648c45175f354170b
kingjaymane <jieshinmercado@gmail.com>
2023-11-27 14:46:28 -0500

    react chat engine for campus keeps messaging

--- campuskeep-frontend/pages/message.js
+++ campuskeep-frontend/pages/message.js
@@ -1,31 +1,26 @@
 import Navbar from '../components/Navbar';
-import { Box, Text, VStack, ChakraProvider } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
+import ChatEngineCore from 'react-chat-engine'; 
+import ChatEngineAdvanced from 'react-chat-engine-advanced';
 
-const Messaging = () => {
-    return (
-      <div>
-        <Head>
-          <title>Messaging</title>
-        </Head>
-        <div className="container mx-auto p-4">
-          <h1 className="text-3xl font-bold mb-4">Messaging App</h1>
-          <div className="flex">
-            <div className="w-1/4 p-4 border-r">
-              <h2 className="text-xl font-bold mb-2">Conversations</h2>
-              <ul>
-                <li className="cursor-pointer hover:bg-gray-100 p-2">User 1</li>
-                <li className="cursor-pointer hover:bg-gray-100 p-2">User 2</li>
-              </ul>
-            </div>
-            <div className="w-3/4 p-4">
-              <h2 className="text-xl font-bold mb-2">Selected Conversation</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
-  
-  export default Messaging;
+const CampusKeepsMessages = () => {
+
+  return (
+    <ChatEngineAdvanced
+      projectID="lost_and_found_project" 
+      userName="admin" 
+      userSecret="123456"
+      height="100vh"
+      customParsers={[
+        
+      ]}
+      customMiddleware={[
+        
+      ]}
+    />
+  );
+
+};
+
+export default CampusKeepsMessages;
